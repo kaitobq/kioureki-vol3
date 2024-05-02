6b6ea5e52d62529dac1326981982f368d793eb28
kaitobq <kaitobq520@gmail.com>
2024-05-02 15:51:05 +0900

    feat: routes.rb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace "api" do
+    resources :users, only: %i[update]
+    # resources :users, only: %i[create, update]
+    post "users", to: "users#create"
+    post "login", to: "authentication#login"
+    post "validate", to: "authentication#validate"
+  end
 end
