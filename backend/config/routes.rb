ff013ffe1327aded62111bdab0a4aac4c6e2c1cf
kaitobq <kaitobq520@gmail.com>
2024-05-03 10:39:46 +0900

    feat: added organizations, memberships routing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  get 'memberships/create'
+  get 'organizations/create'
   namespace "api" do
     resources :users, only: %i[update]
     # resources :users, only: %i[create, update]
     post "users", to: "users#create"
     post "login", to: "authentication#login"
     post "validate", to: "authentication#validate"
+    # namespace "organizations" do
+    #   post "index", to: "organizations#index"
+    #   post "create", to: "organizations#create"
+    # end
+    # resources :organizations, only: [:index]
+    # resources :organizations, only: [:create]
+    resources :organizations, only: [:index, :create]
+    resources :memberships, only: [:create]
   end
 end
