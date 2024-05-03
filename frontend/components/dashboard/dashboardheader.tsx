06d659e4a45a8b81ec753911b6560fc4957af7ca
kaitobq <kaitobq520@gmail.com>
2024-05-03 23:04:11 +0900

    feat: query parameter

--- frontend/components/dashboard/dashboardheader.tsx
+++ frontend/components/dashboard/dashboardheader.tsx
@@ -1,17 +1,27 @@
-import React, { FC, useState } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import Link from "next/link";
 import { FaUserInjured } from "react-icons/fa";
 import { Organization } from "@/types/organization";
+import { useSearchParams } from "react-router-dom";
 
 interface DashboardHeaderProps {
   organizations: Organization[];
+  setCurrentOrganization: (org: Organization) => void;
+  currentOrganization: Organization | null;
 }
 
-const DashboardHeader: React.FC<DashboardHeaderProps> = ({ organizations }) => {
-  const [currentOrganization, setCurrentOrganization] = useState<Organization>(
-    organizations[0]
-  );
+const DashboardHeader: React.FC<DashboardHeaderProps> = ({
+  organizations,
+  setCurrentOrganization,
+  currentOrganization,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSetCurrentOrganization = (org: Organization) => {
+    setCurrentOrganization(org);
+    setSearchParams({ organization_id: org.id.toString() });
+  };
 
   return (
     <div className="flex flex-col justify-center items-center my-3 bg-transparent">
@@ -21,24 +31,23 @@ const DashboardHeader: React.FC<DashboardHeaderProps> = ({ organizations }) => {
             <FaUserInjured size={30} />
             <span className="text-lg font-bold">Kioureki</span>
           </Link>
-
           <div className="relative">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow transition-all transform hover:scale-105"
               onClick={() => setIsOpen(!isOpen)}
             >
-              {currentOrganization.name}
+              {currentOrganization
+                ? currentOrganization.name
+                : "Select Organization"}
+              {isOpen ? "↑" : "↓"}
             </button>
             {isOpen && (
               <ul className="absolute bg-red-600 shadow-lg rounded w-auto flex flex-col items-center left-1/2 transform -translate-x-1/2">
                 {organizations.map((org) => (
                   <li
                     key={org.id}
-                    className={`m-1 px-4 py-2 cursor-pointer rounded-lg ${currentOrganization === org ? "bg-gray-800  hover:bg-gray-600" : "bg-blue-800 hover:bg-blue-600"}`}
-                    onClick={() => {
-                      setCurrentOrganization(org);
-                      setIsOpen(false);
-                    }}
+                    className={`m-1 px-4 py-2 cursor-pointer rounded-lg ${currentOrganization && currentOrganization.id === org.id ? "bg-gray-800 hover:bg-gray-600" : "bg-blue-800 hover:bg-blue-600"}`}
+                    onClick={() => handleSetCurrentOrganization(org)}
                   >
                     {org.name}
                   </li>
