85a982a8fd1736fccff6f1b363a8bafb00c9b236
kaitobq <kaitobq520@gmail.com>
2024-05-03 11:25:37 +0900

    refactor: added check for duplicates

--- backend/app/controllers/api/memberships_controller.rb
+++ backend/app/controllers/api/memberships_controller.rb
@@ -2,6 +2,12 @@ class Api::MembershipsController < ApplicationController
   before_action :authenticate  # ユーザー認証を行う
 
   def create
+    # 重複をチェック
+    existing_membership = Membership.find_by(user_id: @current_user.id, organization_id: membership_params[:organization_id])
+    if existing_membership
+      render json: { status: 'error', message: 'You are already a member of this organization' }, status: :unprocessable_entity
+      return
+    end
     # @current_userはauthenticateメソッドでセットされる
     membership = Membership.new(membership_params.merge(user_id: @current_user.id))
     if membership.save
