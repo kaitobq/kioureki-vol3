97f8da5a935d7c15b682831c5f98263bfb889e0f
kaitobq <kaitobq520@gmail.com>
2024-05-10 10:34:24 +0900

    feat: add create organization page

---
+++ frontend/app/organization/new/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import OrganizationHeader from "@/components/organization/organization-header";
+
+const CreateOrganizationPage = () => {
+  return (
+    <div className="w-full h-screen">
+      <h3>ご利用のために団体を作成または参加する必要があります。</h3>
+      <button>作成</button>
+      <button>参加</button>
+    </div>
+  );
+};
+
+export default CreateOrganizationPage;
