68a50d776073e6048e43fa597865c9f27a90ca6c
kaitobq <kaitobq520@gmail.com>
2024-05-11 21:48:47 +0900

    refactor: change logo size

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -7,8 +7,8 @@ const NavBar = () => {
       <header className="bg-gradient-to-r from-gray-800 to-blue-800 text-white w-4/5 sm:w-3/5 rounded-full shadow-md shadow-black">
         <div className="container mx-auto flex justify-between items-center py-4 px-6">
           <Link href={"/"} className="flex items-center">
-            <FaUserInjured size={30} />
-            <span className="text-lg font-bold">Kioureki</span>
+            <FaUserInjured size={25} />
+            <span className="text-xl font-bold">Kioureki</span>
           </Link>
           <Link href="/signin" className="" legacyBehavior>
             <a className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow transition-all transform hover:scale-105">
