2f79c53f094f288826c5a8a027adc9044682436e
kaitobq <kaitobq520@gmail.com>
2024-05-11 19:22:28 +0900

    rafactor: responsive ui

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -4,16 +4,12 @@ import { FaUserInjured } from "react-icons/fa";
 const NavBar = () => {
   return (
     <div className="flex flex-col justify-center items-center my-3 bg-transparent">
-      <header className="bg-gradient-to-r from-gray-800 to-blue-800 text-white w-3/5 rounded-full shadow-md shadow-black">
+      <header className="bg-gradient-to-r from-gray-800 to-blue-800 text-white w-4/5 sm:w-3/5 rounded-full shadow-md shadow-black">
         <div className="container mx-auto flex justify-between items-center py-4 px-6">
           <Link href={"/"} className="flex items-center">
             <FaUserInjured size={30} />
             <span className="text-lg font-bold">Kioureki</span>
           </Link>
-
-          {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow transition-all transform hover:scale-105">
-            Sign Up
-          </button> */}
           <Link href="/signin" className="" legacyBehavior>
             <a className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow transition-all transform hover:scale-105">
               Sign In
