5661812d50c4f237357dda5d46740367620fc5b7
kaitobq <kaitobq520@gmail.com>
2024-05-11 19:22:15 +0900

    refactor: responsive ui

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -21,9 +21,8 @@ export default function HomePage() {
   return (
     <div className="bg-gradient-to-r from-blue-800 to-cyan-500 h-screen flex flex-col  text-white">
       <NavBar />
-
-      <div className="w-full px-40 py-16 flex flex-col justify-center">
-        <h1 className="text-5xl font-bold mb-4">
+      <div className="w-full my-16 sm:my-48 xl:my-12 px-10 sm:px-40 py-16 flex flex-col text-center items-center">
+        <h1 className="text-3xl sm:text-6xl font-bold m-4">
           {token ? "Welcome back to Kioureki" : "Welcome to Kioureki"}
         </h1>
         <p className="text-xl mb-8">
@@ -34,7 +33,7 @@ export default function HomePage() {
           className=""
           legacyBehavior
         >
-          <a className="inline-block w-2/5 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg shadow-lg transition-all transform hover:scale-110">
+          <a className="inline-block w-3/5 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg shadow-lg transition-all transform hover:scale-110">
             Get Started →
           </a>
         </Link>
