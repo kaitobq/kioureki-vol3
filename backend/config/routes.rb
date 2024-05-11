c7b34387175d089dbb67902e88b47edcc1ee02dc
kaitobq <kaitobq520@gmail.com>
2024-05-11 17:39:57 +0900

    feat: invitations route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'invitations/create'
+  end
   namespace "api" do
     resources :users, only: %i[update]
     # resources :users, only: %i[create, update]
@@ -11,8 +14,12 @@ Rails.application.routes.draw do
     # end
     # resources :organizations, only: [:index]
     # resources :organizations, only: [:create]
-    resources :organizations, only: [:index, :create]
+    resources :organizations, only: [:index, :create] do
+      resources :invitations, only: [:create]
+    end
     resources :memberships, only: [:create]
+
+    post 'invitations/accept', to: 'memberships#create'
 
     resources :medical_records, only: [:index, :show, :create, :update, :destroy]
   end
