f5f389f285f118543e90d100540617b3d859058b
kaitobq <kaitobq520@gmail.com>
2024-05-12 13:10:54 +0900

    refactor: remove comments

--- frontend/components/organization/organization-header.tsx
+++ frontend/components/organization/organization-header.tsx
@@ -6,7 +6,6 @@ import { activeMenus } from "@/app/organization/page";
 
 interface OrganizationHeaderProps {
   organizations: Organization[];
-  setCurrentOrganization: (org: Organization) => void;
   currentOrganization: Organization | null;
   active: activeMenus;
   setActive: (menu: activeMenus) => void;
@@ -14,7 +13,6 @@ interface OrganizationHeaderProps {
 
 const OrganizationHeader: React.FC<OrganizationHeaderProps> = ({
   organizations,
-  setCurrentOrganization,
   currentOrganization,
   active,
   setActive,
@@ -35,8 +33,6 @@ const OrganizationHeader: React.FC<OrganizationHeaderProps> = ({
   );
 
   const handleSetCurrentOrganization = (org: Organization) => {
-    setCurrentOrganization(org);
-    // setSearchParams({ organization_id: org.id.toString() });
     router.push(
       pathname + "?" + createQueryString("organization_id", org.id.toString())
     );
@@ -52,29 +48,40 @@ const OrganizationHeader: React.FC<OrganizationHeaderProps> = ({
         <div className="h-5 my-2 mx-5 flex items-center gap-5">
           <button
             onClick={() => setActive("Database")}
-            className={`font-semibold text-sm ${active === "Database" ? "border-b-4 border-blue-700" : null}`}
+            className={`font-semibold text-sm ${
+              active === "Database" ? "border-b-4 border-blue-700" : null
+            }`}
           >
             Database
           </button>
           <button
             onClick={() => setActive("Setting")}
-            className={`font-semibold text-sm ${active === "Setting" ? "border-b-4 border-blue-700" : null}`}
+            className={`font-semibold text-sm ${
+              active === "Setting" ? "border-b-4 border-blue-700" : null
+            }`}
           >
             Setting
           </button>
           <div className="flex flex-col items-center px-5 font-semibold">
             <button onClick={() => setIsOpen(!isOpen)}>
-              {currentOrganization
-                ? currentOrganization.name
-                : "Select Organization"}
-              {isOpen ? "↑" : "↓"}
+              {currentOrganization ? (
+                currentOrganization.name
+              ) : (
+                <h3 className="font-semibold text-sm">
+                  Select Organization{isOpen ? "↑" : "↓"}
+                </h3>
+              )}
             </button>
             {isOpen && (
               <ul className="flex flex-col items-center absolute mt-7 text-center bg-white shadow-lg rounded border font-semibold text-sm">
                 {organizations.map((org) => (
                   <li
                     key={org.id}
-                    className={`w-fit min-w-20 m-0.5 px-2 py-0.5 cursor-pointer rounded-md ${currentOrganization && currentOrganization.id === org.id ? "bg-blue-700 text-white hover:bg-gray-600" : "bg-white hover:shadow-lg hover:bg-gray-200"}`}
+                    className={`w-fit min-w-20 m-0.5 px-2 py-0.5 cursor-pointer rounded-md ${
+                      currentOrganization && currentOrganization.id === org.id
+                        ? "bg-blue-700 text-white hover:bg-gray-600"
+                        : "bg-white hover:shadow-lg hover:bg-gray-200"
+                    }`}
                     onClick={() => handleSetCurrentOrganization(org)}
                   >
                     {org.name}
