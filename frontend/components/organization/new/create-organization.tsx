d44d21d3b16131f787327a22b1b8968b6ae14bf1
kaitobq <kaitobq520@gmail.com>
2024-05-12 14:48:14 +0900

    fix: label

--- frontend/components/organization/new/create-organization.tsx
+++ frontend/components/organization/new/create-organization.tsx
@@ -38,7 +38,7 @@ const CreateOrganizationPage = () => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center p-10">
-      <h3 className="text-2xl font-bold mb-6">団体の作成</h3>
+      <h3 className="text-2xl font-bold mb-6">組織の作成</h3>
       <form
         onSubmit={handleSubmit}
         className="flex flex-col items-center w-full max-w-md relative z-0"
@@ -55,7 +55,7 @@ const CreateOrganizationPage = () => {
           htmlFor="organization_name"
           className="left-0 peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 rtl:peer-focus:right-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
-          団体名
+          組織名
         </label>
         <button
           type="submit"
