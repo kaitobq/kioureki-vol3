c014c2e87b1c13a64020a97bff80bd2fbdbb57fd
kaitobq <kaitobq520@gmail.com>
2024-05-12 16:08:39 +0900

    fix: cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins "kioureki-vol3.vercel.app"
+      # origins "localhost:3001"
 
       resource "*",
         headers: :any,
