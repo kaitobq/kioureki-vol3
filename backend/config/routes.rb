2bad47427e7b29f33f52fa14cbc65b690c55bb81
kaitobq <kaitobq520@gmail.com>
2024-05-12 15:06:09 +0900

    refactor: remove comment

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,16 +4,11 @@ Rails.application.routes.draw do
   end
   namespace "api" do
     resources :users, only: %i[update]
-    # resources :users, only: %i[create, update]
+
     post "users", to: "users#create"
     post "login", to: "authentication#login"
     post "validate", to: "authentication#validate"
-    # namespace "organizations" do
-    #   post "index", to: "organizations#index"
-    #   post "create", to: "organizations#create"
-    # end
-    # resources :organizations, only: [:index]
-    # resources :organizations, only: [:create]
+
     resources :organizations, only: [:index, :create] do
       resources :invitations, only: [:create]
     end
